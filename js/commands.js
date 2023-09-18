936b1e57a6ed2bc00cea7b97a457adc1583c7ac0
Greggert <103889062+Greggert@users.noreply.github.com>
2023-09-18 01:17:31 -0400

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,316 @@
+var youtube = "https://www.youtube.com/TheSavageMuzik/";
+var twitter = "https://twitter.com/Strafford1Tech";
+var linkedin = "https://www.linkedin.com/in/adolphus-strafford-iii-86a0b725a/";
+var github = "https://github.com/Greggert";
+var parchment = "https://www.parchment.com/u/award/889637995cf18982bbc35d271e8124d3";
+var resume = "https://drive.google.com/uc?export=download&id=1QV0P2uuk1GAS2rgEpMg1yaMr2qa8c-My";
+var email = 'mailto:astrafford52@gmail.com';
+
+whois = [
+  "<br>",
+
+  "             _           _     ",
+  "   __      _| |__   ___ (_)___  ",
+  "   \\ \\ /\\ / / '_ \\ / _ \\| / __| ",
+  "    \\ V  V /| | | | (_) | \\__ \\  ",
+  "     \\_/\\_/ |_| |_|\\___/|_|___/   ",
+                      
+  
+  "<br>",                            
+  //break every sentece after technology
+  "Welcome to my Who Is Adolphus section, where I'm thrilled to introduce myself and share my intense interest for technology ",
+  " and cybersecurity. I recently earned a Bachelor of Science in Information Technology with a cybersecurity concentration from ",
+  " the University of Central Florida (UCF). With this strong educational background and a strong drive to safeguard digital assets, ",
+  " I'm excited to start a fulfilling career in cybersecurity. My qualifications include a range of certifications such as Security+, ",
+  " Cisco Packet Tracer, IT Fundamentals, and IC3. These certifications have equipped me with a comprehensive understanding of various ",
+  " cybersecurity concepts and the underlying technologies. They have also honed my skills in network security, systems administration, ",
+  " and fundamental computer principles. ",
+  "<br>",
+  "Beyond the classroom, my dedication to cybersecurity is a continuous journey.", 
+  "I'm a perpetual learner, constantly seeking opportunities to explore the latest industry trends and potential threats.", 
+  "I actively engage in cybersecurity challenges and assignments, constantly refining my skills and expanding my knowledge.",
+  "My ultimate aspiration is to become a penetration tester, enabling organizations to proactively identify and address ",
+  "system vulnerabilities. The notion of responsibly probing systems to uncover security flaws is both intriguing and rewarding to me.", 
+  "Through this work, I aim to protect critical data and contribute to the advancement of cybersecurity practices on a larger scale.",
+  "I am a motivated and passionate individual who thrives in dynamic environments that encourage teamwork, critical thinking, ",
+  "and innovation. My technical expertise, problem-solving skills, and unwavering commitment to lifelong learning make me a  ",
+  "valuable asset to any organization.",
+  "<br>",  
+  "I am actively seeking opportunities where I can apply my skills and make a real impact in the field of cybersecurity.",  
+  "If you'd like to connect or explore potential collaborations, please don't hesitate to reach out.", 
+  "I eagerly look forward to networking with fellow experts who share my enthusiasm for computers and cybersecurity.",
+  "In addition to my cybersecurity background, I have developed a diverse set of technical skills during my academic journey.", 
+  "This includes proficiency in programming languages such as C++ and JavaScript, along with expertise in ",
+  "Object-Oriented Programming (OOP). I have also gained practical experience with various tools and technologies,", 
+  "including AWS Cloud, Cisco Packet Tracer, Kali Linux, Parrot OS, Oracle Virtual Box, VMware, MySQL Workbench,  ",
+  "WireShark, and Microsoft Azure. My professional experience as a IT Technical at Grant Access Secure Services in  ",
+  "Lakeland, Florida, has been invaluable. In this role, I collaborated with a small business owner to set up  ",
+  "multiple work computers, evaluated security risks, and provided streamlined usage and maintenance recommendations.", 
+  "I also administered software installations, ensuring the efficient functioning of systems.",
+  "<br>",
+  "Additionally, my role as an Information Security Engineer/IT Technician at Mr. B's Grooming Lounge in Orlando,", 
+  "Florida, involved implementing security measures to safeguard sensitive customer and payment data.", 
+  "I utilized encryption techniques with AES and configured firewalls to enhance network protection. Furthermore,", 
+  "I integrated MySQL with their Square POS system, streamlining the input of client and payment details.",
+  "As I embark on my professional journey, my commitment to continuous learning and development in the field of ",
+  "information technology remains unwavering. I am enthusiastic about utilizing my skills to support organizations ",
+  "and initiatives that make a significant positive impact while continually pursuing innovation and excellence.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+
+
+
+
+  "                   _       _  ",
+  "    ___  ___   ___(_) __ _| |  ",
+  "   / __|/ _ \\ / __| |/ _` | | ",
+  "   \\__ \\ (_) | (__| | (_| | |  ",
+   "   |___/\\___/ \\___|_|\\__,_|_|  ",
+                           
+ 
+ 
+                             
+ "<br>",
+
+
+  'linkedin       <a href="' + linkedin + '" target="_blank">Linkedin (open in new tab)' + "</a>",
+  'github         <a href="' + github + '" target="_blank">Github (open in new tab)' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">Twitter (open in new tab)' + '</a>',
+  "<br>"
+];
+
+resume = [
+  "<br>",
+
+
+
+
+                                    
+  "    _ __ ___  ___ _   _ _ __ ___   ___  ",
+  "   | '__/ _ \\/ __| | | | '_ ` _ \\ / _ \\ ",
+  "   | | |  __/\\__ \\ |_| | | | | | |  __/ ",
+  "   |_|  \\___||___/\\__,_|_| |_| |_|\\___| ",
+                                      
+  
+  
+
+
+
+  "<br>",
+  'resume       <a href="' + resume + '" target="_blank">Resume (open in new tab/download)' + "</a>",
+  "<br>"
+];
+
+certs = [
+  "<br>",
+
+  
+
+  "                 _   _  __ _           _   _                   ",
+  "    ___ ___ _ __| |_(_)/ _(_) ___ __ _| |_(_) ___  _ __  ___   ",
+  "   / __/ _ \\ '__| __| | |_| |/ __/ _` | __| |/ _ \\| '_ \\/ __|  ",
+  "  | (_|  __/ |  | |_| |  _| | (_| (_| | |_| | (_) | | | \\__ \\  ",
+  "   \\___\\___|_|   \\__|_|_| |_|\\___\\__,_|\\__|_|\\___/|_| |_|___/  ",
+                                                           
+
+
+                                                       
+
+
+"<br>",
+
+
+  "CompTIA Security +",
+  "Cisco Introduction to Packet Tracer",
+  "Ceriport IC3 Digital Literacy",
+  "TestOut IT Fundamentals Pro",
+  "<br>"
+];
+
+work = [
+  "<br>",
+
+
+
+  
+
+
+
+  "                       _     ",
+  "   __      _____  _ __| | __  ",
+  "   \\ \\ /\\ / / _ \\| '__| |/ /  ",
+  "    \\ V  V / (_) | |  |   <   ",
+  "     \\_/\\_/ \\___/|_|  |_|\\_\\  ",
+                           
+  
+  
+                           
+ "<br>",
+ 
+"Grant Access Secure Services	Lakeland, FL",
+  "IT Technician	01/2022 – Present",
+   "o	Teamed up with a small business owner to create and set up multiple work computers." ,
+   "o	Evaluated security risks, offered streamlined usage and maintenance suggestions for the organization.",
+   "o	Administered system’s Programs/Software Installation.",
+   "<br>",
+   "<br>",
+"Mr. B’s Grooming Lounge	Orlando, FL",
+  "Information Security Engineer/ IT Technician	06/2017 – Present",
+   "o	Implement security measures to protect sensitive customer and payment data.",
+   "o	Encrypt data with AES, set up firewalls for network protection.",
+   "o	Downloaded and installed the Square POS app on all the devices designated for payment processing.",
+   "o	To expedite the input of client and payment details, I integrated MySQL with their Square POS system.",
+
+  "<br>"
+];
+
+skills = [
+  "<br>",
+
+  
+
+
+  "        _    _ _ _      ",
+  "    ___| | _(_) | |___  ",
+  "   / __| |/ / | | / __| ",
+  "   \\__ \\   <| | | \\__ \\ ",
+  "   |___/_|\\_\\_|_|_|___/ ",
+                     
+ 
+ 
+                       
+
+"<br>",
+
+
+  "Languages",
+	"Intermediate:	HTML, CSS/SCSS, JavaScript, C++, Java, Object Oriented Programming",
+	"Basic:	Python, T-SQL, MySQL, XML, JSON, Typescript, C#",
+	"<br>",
+  "<br>",
+	"Tools",
+	"Intermediate: Oracle VirtualBox, VMware, Atom, Cisco Packet Tracer, Wireshark, Bootstrap, PowerShell, Notepad++", 
+	"Basic:	VS Code, Git, Apache Maven, MobaXterm, MySQL Workbench",
+	"<br>",
+  "<br>",
+	"OS",
+	"Intermediate: Windows, Linux, macOS, iOS, Kali Linux/Parrot OS, Microsoft Windows Server",
+	"Basic: Ubuntu, Debian, Android",
+  "<br>",
+  "<br>",
+	"IDE's",
+	"Intermediate: CLion, Visual Studio, IntelliJ, PyCharm",
+	"Basic: NetBeans, Eclipse, Microsoft SQL Server Management Studio",
+	"<br>",
+  "<br>",
+	"Cloud Platforms",
+	"Intermediate: Aws Cloud, Microsoft Azure",
+	"Basic: Google Cloud, Oracle",
+
+
+  "<br>"
+];
+
+projects = [
+  "<br>",
+
+    
+
+
+
+  "                    _           _        ",
+  "    _ __  _ __ ___ (_) ___  ___| |_ ___  ",
+  "   | '_ \\| '__/ _ \\| |/ _ \\/ __| __/ __|  ",
+  "   | |_) | | | (_) | |  __/ (__| |_\\__ \\  ",
+    "   | .__/|_|  \\___// |\\___|\\___|\\__|___/  ",
+    "   |_|           |__/                     ",
+ 
+ 
+"<br>",
+
+
+  "Monitor and Detection Lab",
+  "●	Designed an Active Directory environment alongside Kali Linux, Security Onion, Splunk and pfSense to simulate ",
+  "a small enterprise network practicing incident response.",
+  "<br>",
+  "Creating a website for personal portfolio using html and custom CSS https://StraffordTech.com",
+  "●	Developed and designed a personal website showcasing my portfolio, skills, and professional achievements to ",
+  "effectively highlight my personal brand and expertise.",
+  "<br>",
+  "Network-Wide Ad Blocking with Pi-hole ",
+  "●	Deployed a streamlined network-wide ad-blocking solution with Pi-hole on a Raspberry Pi server, reinforcing ",
+  "online privacy and minimizing intrusive ads, and conducted system maintenance through PuTTY SSH. ",
+  "<br>"
+  
+];
+
+help = [
+  "<br>",
+
+      
+
+  
+
+
+                  " __         __         ",
+                  " | |__   ___| |_ __   ",
+                  " | '_ \\ / _ \\ | '_ \\  ",
+                  " | | | |  __/ | |_) | ",
+                  " |_| |_|\\___|_| .__/  ",
+                  "             |_|      ",
+  
+  
+
+
+ 
+  "<br>",
+
+  '<span class="command">whois</span>          Who is Adolphus?',
+  '<span class="command">degree</span>         View UCF Degree',
+  '<span class="command">resume</span>         View Resume',
+  '<span class="command">certs</span>          View Current Certifications',
+  '<span class="command">work</span>           View Work Experience',
+  '<span class="command">skills</span>         View My Skill List',
+  '<span class="command">video</span>          View YouTube Videos',
+  '<span class="command">social</span>         Display Social Networks',
+  '<span class="command">projects</span>       View Projects',
+  '<span class="command">history</span>        View Command History',
+  '<span class="command">help</span>           List of Commands',
+  '<span class="command">email</span>          Email Me',
+  '<span class="command">clear</span>          Clear Terminal',
+  "<br>",
+];
+
+video = [
+  "<br>",
+  '<span class="command">access</span>           Type Access To Enter',
+  "<br>"
+];
+
+clear = [
+  "<br>",
+  '<span class="index"></span>',
+  "                 _nnnn_                  ",
+  "                dGGGGMMb            ",
+  "               @p~qp~~qMb           ",
+  "               M|@||@) M|            ",
+  "               @,----.JM|     ____    __                      ___    ___                __      ______               __  ",
+  "              JS^\__/  qKL    /\\  _`\\ /\\ \\__                 /'___\\ /'___\\              /\\ \\    /\\__  _\\             /\\ \\      ",
+  "             dZP        qKRb \\ \\,\\L\\_\\ \\ ,_\\  _ __    __   /\\ \\__//\\ \\__/  ___   _ __  \\_\\ \\   \\/_/\\ \\/    __    ___\\ \\ \\___   ",
+  "            dZP          qKKb \\/_\\__ \\\\ \\ \\/ /\\`'__\\/'__`\\ \\ \\ ,__\\ \\ ,__\\/ __`\\/\\`'__\\/'_` \\     \\ \\ \\  /'__`\\ /'___\\ \\  _ `\\   ",
+  "           fZP            SMMb  /\\ \\L\\ \\ \\ \\_\\ \\ \\//\\ \\L\\.\\_\\ \\ \\_/\\ \\ \\_/\\ \\L\\ \\ \\ \\//\\ \\L\\ \\     \\ \\ \\/\\  __//\\ \\__/\\ \\ \\ \\ \\    ",
+  "           HZM            MMMM  \\ `\\____\\ \\__\\\\ \\_\\\\ \\__/.\\_\\\\ \\_\\  \\ \\_\\  \\____/\\ \\_\\\\ \\___,_\\     \\ \\_\\ \\____\\ \\____\\\\ \\_\\ \\_\\   ",
+  "           FqM            MMMM   \\/_____/\\/__/ \\/_/ \\/__/\\/_/ \\/_/   \\/_/ \\/___/  \\/_/ \\/__,_ /      \\/_/\\/____/\\/____/ \\/_/\\/_/  ",
+  "         __|  .        |\\dS qML     ",
+  "        |    `.       | `' \\Zq                       IT|CyberSecurity Professional         ",
+  "       _)      \\.___.,|     .'                               Web Developer  ",
+  "       \\____   )MMMMMP|   .'  ",
+  "           `-'       `--'                  ",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<br>",
+
+];
